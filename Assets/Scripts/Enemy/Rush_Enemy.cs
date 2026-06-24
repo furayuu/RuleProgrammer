af612da94b695e210257e3353dd088b8bac5a08d
furayuu <ruskchanlove@gmail.com>
2026-06-24 14:04:54 +0900

    Rush_Enemyを作成、プレイヤーの3メートル以内に入った場合速度が倍になる ※スクリプトの場所を一部移動させました、具体的にはプレイヤー関連とエネミー関連をフォルダに分類分けしました

--- Assets/Scripts/Enemy/Rush_Enemy.cs
+++ Assets/Scripts/Enemy/Rush_Enemy.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Rush_Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Transform playerTr; // プレイヤーのTransform
+    [SerializeField] float speed = 2; // 敵の動くスピード
+    private Animator animator;
+
+    private void Start()
     {
-        
+        // プレイヤーのTransformを取得（プレイヤーのタグをPlayerに設定必要）
+        playerTr = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        // プレイヤーとの距離が0.1f未満になったらそれ以上実行しない
+        if (Vector2.Distance(transform.position, playerTr.position) < 0.1f)
+            return;
+
+        if (Vector2.Distance(transform.position, playerTr.position) < 3.0f)
+        {
+            speed = 4.0f;
+        }
+
+            // プレイヤーに向けて進む
+            transform.position = Vector2.MoveTowards(
+            transform.position,
+            new Vector2(playerTr.position.x, playerTr.position.y),
+            speed * Time.deltaTime);
+
+        animator.SetBool("IsRunning", true);
     }
 }
